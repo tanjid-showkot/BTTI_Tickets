506a14d4e8125f484637e99b38a71c3f39d51671
tanjid-showkot <tanjidsaykot611@gmail.com>
2025-02-07 19:32:59 +0600

    pwa

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import webRouter from "./Router/Router.jsx";
 import { RouterProvider } from "react-router";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Reload?")) {
+      updateSW();
+    }
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={webRouter} />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: 'My PWA App',
+      short_name: 'PWA',
+      description: 'My Progressive Web App',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    }
+  })],
 })
