496769026869ac6e6f384599adea575e37791f6b
tanjid-showkot <tanjidsaykot611@gmail.com>
2025-03-04 21:55:16 +0600

    Pwa_icons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.d3lo08qe6v8"
+    "revision": "0.vr890f78ido"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
